8f8e3d954a4e2e7dd618d24e2cec74c474d98804
JUNE-29 <jiyuni529@gmail.com>
2022-03-30 15:28:52 +0900

    [add] JS: button scroll function

--- main.js
+++ main.js
@@ -13,3 +13,46 @@ document.addEventListener('scroll', () => {
     }
 });
 
+const homeBtn = document.querySelector('.Home__button');
+const home = document.querySelector('#home');
+
+homeBtn.addEventListener('click', () => {
+    home.scrollIntoView({behavior: "smooth", block: "center", inline:"nearest"}); 
+});
+
+
+
+const aboutBtn = document.querySelector('.about__button');
+const about = document.querySelector('#about');
+
+aboutBtn.addEventListener('click', () => {
+    about.scrollIntoView({behavior: "smooth", block: "center", inline:"nearest"}); 
+});
+
+
+const skillsBtn=  document.querySelector('.skills__button');
+const skills = document.querySelector('#skills');
+
+skillsBtn.addEventListener('click', ()=> {
+    skills.scrollIntoView({behavior: "smooth", block: "center", inline:"nearest"});
+});
+
+
+const workBtn=  document.querySelector('.work__button');
+const work = document.querySelector('#work');
+
+workBtn.addEventListener('click', ()=> {
+    work.scrollIntoView({behavior: "smooth", block: "center", inline:"nearest"});
+});
+
+const contactBtn=  document.querySelector('.contact__button');
+const contact = document.querySelector('#contact');
+const homeContactBtn = document.querySelector(".home__contact");
+
+contactBtn.addEventListener('click', ()=> {
+    contact.scrollIntoView({behavior: "smooth", block: "center", inline:"nearest"});
+});
+
+homeContactBtn.addEventListener('click', ()=> {
+    contact.scrollIntoView({behavior: "smooth", block: "center", inline:"nearest"});
+});
